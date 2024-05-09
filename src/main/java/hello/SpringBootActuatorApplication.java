f2e8af2d7509b7816bca9d0ed4e466057a768331
LimJiNa <jn4624@gmail.com>
2024-05-09 20:37:27 +0900

    HTTP 요청 응답 기록 설정

--- src/main/java/hello/SpringBootActuatorApplication.java
+++ src/main/java/hello/SpringBootActuatorApplication.java
@@ -1,13 +1,20 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootActuatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootActuatorApplication.class, args);
+	}
+
+	@Bean
+	public InMemoryHttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 
 }
